54a9af1834dc740e502e3234353a0ce050df5d42
Jenishp15 <jenishpatel1816@gmail.com>
2024-07-01 17:41:07 +0530

    new

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import UserContextProvider from './context/UserContextProvider.jsx'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+
+
+  return (
+    <UserContextProvider>
+     <h1>React aur chai</h1>
+     <Login />
+     <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
